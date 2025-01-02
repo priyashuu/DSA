c4d5869d3a5402a81b8072a73284c9b889659cba
priya <priyaaashu4@gmail.com>
2025-01-03 01:52:13 +0530

    changes

--- LinkedList/SinglyLinkedList/Insertion/InstertionAtEnd/Insertion.java
+++ LinkedList/SinglyLinkedList/Insertion/InstertionAtEnd/Insertion.java
@@ -1,40 +1,55 @@
 public class Insertion {
    public static class Node {
-       int data;
-       Node next;
-       Node(int data) {
-           this.data=data;
-       }
+      int data;
+      Node next;
+
+      Node(int data) {
+         this.data = data;
+      }
    }
+
    public static class LinkedList {
-      Node head= null;
-      Node tail =  null ;
-      void insertAtEnd(int val ){
-         Node temp =new Node (val);
-         if (head == null){
-            head=temp;
-            
+      Node head = null;
+      Node tail = null;
+
+      void insertAtEnd(int val) {
+         Node temp = new Node(val);
+         if (head == null) {
+            head = temp;
+
+         } else {
+            tail.next = temp;
+
          }
-         else{
-            tail.next=temp;
-           
-         }
-         tail=temp;
+         tail = temp;
       }
-      void display(){
-         while (head !=null){
+      
+
+      void display() {
+         Node temp = head ;
+         while (temp != null) {
             System.out.println(head.data + " ");
-            head =head.next;
+             temp = temp.next;
          }
+      }
+      int length () {
+         Node temp = head;
+         int count = 0;
+         while(temp!= null ){
+            count ++;
+            temp=temp.next;
+
+         }
+         return count ;
       }
    }
-   
-    
+
    public static void main(String[] args) {
       LinkedList ll = new LinkedList();
       ll.insertAtEnd(7);
       ll.insertAtEnd(6);
       ll.display();
+      System.out.println(ll.length());
 
    }
 
