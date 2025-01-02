e381e65b7b69afd75616d72406144d172798ed77
priya <priyaaashu4@gmail.com>
2025-01-03 01:29:28 +0530

    changes

---
+++ LinkedList/SinglyLinkedList/Insertion/InstertionAtEnd/Insertion.java
@@ -0,0 +1,19 @@
+public class Insertion {
+   public static class Node {
+       int data;
+       Node next;
+       Node(int data) {
+           this.data=data;
+       }
+   }
+   public static class LinkedList {
+      Node head= null;
+      Node tail =  null ;
+   }
+   
+    
+   public static void main(String[] args) {
+
+   }
+
+}
