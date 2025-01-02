4e5440beabb87a794f80412320b39964cdcb3bf1
priya <priyaaashu4@gmail.com>
2025-01-03 01:39:10 +0530

    changes

--- LinkedList/SinglyLinkedList/Insertion/InstertionAtEnd/Insertion.java
+++ LinkedList/SinglyLinkedList/Insertion/InstertionAtEnd/Insertion.java
@@ -9,10 +9,32 @@ public class Insertion {
    public static class LinkedList {
       Node head= null;
       Node tail =  null ;
+      void insertAtEnd(int val ){
+         Node temp =new Node (val);
+         if (head == null){
+            head=temp;
+            
+         }
+         else{
+            tail.next=temp;
+           
+         }
+         tail=temp;
+      }
+      void display(){
+         while (head !=null){
+            System.out.println(head.data + " ");
+            head =head.next;
+         }
+      }
    }
    
     
    public static void main(String[] args) {
+      LinkedList ll = new LinkedList();
+      ll.insertAtEnd(7);
+      ll.insertAtEnd(6);
+      ll.display();
 
    }
 
