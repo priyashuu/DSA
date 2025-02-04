294f0276b257ce9c99f96817ac8ba39720ceea1b
priya <priyaaashu4@gmail.com>
2025-02-04 20:37:35 +0530

    Implement SizeMax and preorder classes for binary tree with node structure and traversal methods

---
+++ Tree/BinaryTree/Implementation/implementation.java
@@ -0,0 +1,44 @@
+package Tree.BinaryTree.Implementation;
+
+public class implementation {
+    public static class Node {
+        int data;
+        Node left; 
+        Node right;
+        public Node(int data) {
+            this.data = data;
+            left = right = null;
+        }
+    }
+    public static void display(Node root){
+        if (root == null) {
+            return;
+        }
+        System.out.print(root.data + " -> ");
+       if(root.left!=null) System.out.print(root.left.data + " ");
+       if(root.right!=null) System.out.print (root.right.data + "  ");
+       System.out.println();
+        display(root.left);
+        display(root.right);
+        
+
+
+
+
+    }
+    public static void main(String[] args) {
+        Node root = new Node(2);
+        Node a = new Node(4);
+        Node b = new Node (10);
+        root.left =a;
+        root.right=b;
+        Node c = new Node(6);
+        Node d = new Node (5);
+        a.left = c;
+        a.right = d;
+        Node f = new Node (11);
+        b.right =f;
+        display(root);
+
+    }
+}
---
+++ Tree/BinaryTree/Traversal/Preorder/preorder.java
@@ -0,0 +1,41 @@
+public class preorder {
+    static int size =0;
+
+    public static class Node {
+        int data;
+        Node left ;
+        Node right;
+        public Node (int data) {
+            this.data = data;
+            left= right = null ;
+
+        }
+
+    }
+    public static void preOrder(Node root ) {
+        if (root==null) return;
+        // System.out.print(root.data + " ");
+        size++;
+        preOrder(root.left);
+        preOrder(root.right);
+    }
+
+    
+    public static void main(String[] args) {
+        Node root = new Node(2);
+        Node a = new Node(4);
+        Node b = new Node (10);
+        root.left =a;
+        root.right=b;
+        Node c = new Node(6);
+        Node d = new Node (5);
+        a.left = c;
+        a.right = d;
+        Node f = new Node (11);
+        b.right =f;
+        
+        preOrder(root);
+       System.out.println(size);
+    }
+    
+}
