5cfdb83d143b2ca64d0d38a4a4dabc70d388b816
priya <priyaaashu4@gmail.com>
2025-02-10 08:45:00 +0530

    Fix Fibonacci sequence base case condition and set default value for n in printfibo method

--- Recursion/Fibonacci/fibo.java
+++ Recursion/Fibonacci/fibo.java
@@ -2,7 +2,7 @@
 
 public class fibo {
     public static void printfibo(int a,int b,int n){
-        if (n==0 || n==1){
+        if (n<=0){
             return ;
         }
         int c=a+b;
@@ -13,7 +13,7 @@ public class fibo {
         int a=0,b=1;
         System.out.println(a);
         System.out.println(b);
-        int n=0;
+        int n=10;
         printfibo(a, b, n-2);
     }
 }
